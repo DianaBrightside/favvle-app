a8e99eb2b8b715daf5109cb5127a1e00eca46ad9
DianaBrightside <31443423+DianaBrightside@users.noreply.github.com>
2021-10-06 15:51:50 +0300

    React router and default styles are added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import SideInfo from "./components/SideInfo";
 import "./styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <SideInfo />
+    </div>
+  );
 }
 
 export default App;
