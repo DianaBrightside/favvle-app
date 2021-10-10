009c1bdb366f6c43a32287beb51a1b0cc9cc8cd9
DianaBrightside <31443423+DianaBrightside@users.noreply.github.com>
2021-10-10 16:47:43 +0300

    Login page and Sign Up page created and firebase connected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
-import SideInfo from "./components/SideInfo";
-import "./styles/App.css";
+import Flexbox from "flexbox-react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import InfoBlock from "./components/InfoBlock";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
+import {
+  LeftColumn,
+  LeftColumnSignUp,
+  RightColumn,
+  RightColumnSignUp,
+} from "./styles/Backgrounds.js/Background";
+
+import GlobalStyle from "./styles/Global";
 
 function App() {
   return (
-    <div className="wrapper">
-      <SideInfo />
-    </div>
+    <Router>
+      <Flexbox flexDirection="row" alignItems="stretch">
+        <Switch>
+          <Route path="/" exact>
+            <LeftColumn
+              as={Flexbox}
+              flex={1}
+              justifyContent="flex-end"
+              minHeight="100vh"
+              alignItems="center"
+            >
+              <InfoBlock />
+            </LeftColumn>
+            <RightColumn
+              as={Flexbox}
+              flex={1}
+              justifyContent="flex-start"
+              minHeight="100vh"
+            >
+              <LoginPage />
+            </RightColumn>
+          </Route>
+          <Route path="/signup">
+            <LeftColumnSignUp
+              as={Flexbox}
+              flex={1}
+              justifyContent="flex-end"
+              minHeight="100vh"
+            >
+              <SignupPage />
+            </LeftColumnSignUp>
+            <RightColumnSignUp
+              as={Flexbox}
+              flex={1}
+              justifyContent="flex-start"
+              minHeight="100vh"
+            >
+              <InfoBlock />
+            </RightColumnSignUp>
+          </Route>
+        </Switch>
+        <GlobalStyle />
+      </Flexbox>
+    </Router>
   );
 }
 
